841b4217a7e1386eec9010996653c0450b35cedc
Klay Clarke <clarkeklay@gmail.com>
2022-12-05 09:01:20 -0500

    audio playback is now functional -> will be good to find a way to allow for different types of audio files to be selected (for now, only wav files are playable)

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,16 +3,19 @@
 //==============================================================================
 MainComponent::MainComponent() : state (Stopped)
 {
+    // open button
     addAndMakeVisible(&openButton);
     openButton.setButtonText("Open...");
     openButton.onClick=[this]{openButtonClicked();};
     
+    // play button
     addAndMakeVisible(&playButton);
     playButton.setButtonText("Play");
     playButton.onClick = [this]{playButtonClicked();};
     playButton.setColour(juce::TextButton::buttonColourId, juce::Colours::green);
     playButton.setEnabled(false);
     
+    // stop button
     addAndMakeVisible(&stopButton);
     stopButton.setButtonText("Stop");
     stopButton.onClick = [this]{stopButtonClicked();};
@@ -47,6 +50,7 @@ MainComponent::~MainComponent()
 }
 
 //==============================================================================
+//![prepareToPlay]
 void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
 {
     // This function will be called when the audio device is started, or when
@@ -54,28 +58,42 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
 
     // You can use this function to initialise any resources you might need,
     // but be careful - it will be called on the audio thread, not the GUI thread.
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
 
     // For more details, see the help for AudioProcessor::prepareToPlay()
 }
+//![prepareToPlay]
 
+//![getNextAudioBlock]
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
     // Your audio-processing code goes here!
-
+    if (MainComponent::readerSource.get() == nullptr)
+    {
+        bufferToFill.clearActiveBufferRegion();
+        return;
+    }
+    
+    transportSource.getNextAudioBlock(bufferToFill);
+    
     // For more details, see the help for AudioProcessor::getNextAudioBlock()
 
     // Right now we are not producing any data, in which case we need to clear the buffer
     // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
+    //bufferToFill.clearActiveBufferRegion();
 }
+//![getNextAudioBlock]
 
+//![releaseResources]
 void MainComponent::releaseResources()
 {
     // This will be called when the audio device stops, or when it is being
     // restarted due to a setting change.
+    MainComponent::transportSource.releaseResources();
 
     // For more details, see the help for AudioProcessor::releaseResources()
 }
+//![releaseResources]
 
 //![openButtonClicked]
 void MainComponent::openButtonClicked()
@@ -171,6 +189,7 @@ void MainComponent::changeState(TransportState newState)
 //![changeState]
 
 //==============================================================================
+//![paint]
 void MainComponent::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
@@ -178,9 +197,10 @@ void MainComponent::paint (juce::Graphics& g)
 
     // You can add your drawing code here!
     g.setColour(juce::Colours::white);
-    g.drawFittedText("Sampler", getLocalBounds(), juce::Justification::centred, 1);
 }
+//![paint]
 
+//![resized]
 void MainComponent::resized()
 {
     // This is called when the MainContentComponent is resized.
@@ -190,6 +210,7 @@ void MainComponent::resized()
     playButton.setBounds(10, 40, getWidth()-20, 20);
     stopButton.setBounds(10, 70, getWidth()-20, 20);
 }
+//![resized]
 
 //![changeListenerCallback]
 void MainComponent::changeListenerCallback(juce::ChangeBroadcaster* source)
